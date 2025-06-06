95e0290651514054182f2889ed9cdd4951b8f910
Victor Reipur <victor@reipur.dk>
2025-06-06 13:50:25 +0200

    Initial local prototype

---
+++ README.md
@@ -0,0 +1,2 @@
+# solid-lamp
+Appen hjælper dig med at få overblik over personer i en roman ved hjælp af kunstig intelligens.
---
+++ app-main/helloworld.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello from helloworld.py inside app-main folder!")
+
+if __name__ == "__main__":
+    main()
